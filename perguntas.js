94955b1c06a9b11fda9f3d89c736e21068934b51
GuiFYK <00001091184252SP@al.educacao.sp.gov.br>
2024-12-05 19:39:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Não sei ',
+    'o q é vidro?',
+    'material inorgânico, homogêneo e amorfo, obtido através da fusão de areia de sílica e outros componentes, e resfriamento posterior da massa'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde esta o Brasil?',
+    'Na America '
+)
+
+criaCartao(
+    'Filosofia',
+    'Ser ou não ser?',
+    'Eis a questão'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é o Universo? ',
+    'Nós? tudo? o q poderia ser de fato?'
+)
